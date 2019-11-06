a056739ce59d6e34e7baabb5800729bfba892329
Ichiro Hiraide <hiraide@atama.plus>
2019-11-06 13:26:40 +0900

    remove Compiler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Compiler, Injector } from '@angular/core';
+import { Component, Injector, ɵcreateInjector as createInjector } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -7,13 +7,13 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private readonly compiler: Compiler, private readonly injector: Injector, private readonly matDialog: MatDialog) {}
+  constructor(private readonly injector: Injector, private readonly matDialog: MatDialog) {}
 
   async openModal() {
     const { ModalModule } = (await import('./modal/modal.module'));
-    const moduleFactory = await this.compiler.compileModuleAsync(ModalModule);
-    const elementModuleRef = moduleFactory.create(this.injector);
-    const { ChildComponent } = elementModuleRef.instance;
+    const injector = createInjector(ModalModule, this.injector);
+    const { ChildComponent } = injector.get(ModalModule);
+
     this.matDialog.open(ChildComponent, {data: {name: 'buko106'}});
   }
 }
