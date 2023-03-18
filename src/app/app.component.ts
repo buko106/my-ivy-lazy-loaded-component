ab8ac452fdd1542fd5fdc092084f4c5916f847bc
Ichiro Hiraide <hiraide@atama.plus>
2023-03-18 13:59:30 +0900

    update material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,33 @@
-import { Component, Injector, ɵcreateInjector as createInjector } from '@angular/core';
+import {
+  Component,
+  Injector,
+  ɵcreateInjector as createInjector,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private readonly injector: Injector, private readonly matDialog: MatDialog) {}
+  constructor(
+    private readonly injector: Injector,
+    private readonly matDialog: MatDialog
+  ) {}
 
   async openModal() {
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    const { ModalModule } = (await import('./modal/modal.module'));
+    const { ModalModule } = await import('./modal/modal.module');
 
     const injector = createInjector(ModalModule, this.injector);
 
     // eslint-disable-next-line @typescript-eslint/naming-convention
     const { ChildComponent } = injector.get(ModalModule);
 
-    this.matDialog.open(ChildComponent, {data: {name: 'buko106'}});
+    const ref = this.matDialog.open(ChildComponent, {
+      data: { name: 'buko106' },
+      panelClass: 'my-mat-dialog-pane',
+    });
   }
 }
--- src/app/modal/child.component.ts
+++ src/app/modal/child.component.ts
@@ -2,8 +2,15 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
-  template: `<span>Hi, {{ data.name }}. I'm lazy loaded child component.</span>`
+  template: `<div>Hi, {{ data.name }}. I'm lazy loaded child component.</div>`,
+  styles: [
+    `
+      div {
+        padding: 24px;
+      }
+    `,
+  ],
 })
 export class ChildComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) readonly data: {name: string}) {}
+  constructor(@Inject(MAT_DIALOG_DATA) readonly data: { name: string }) {}
 }
